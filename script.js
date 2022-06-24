719bcea2bb3f13eba3cea44db70775be8fe26c1e
raplordrds <105345889+raplordrds@users.noreply.github.com>
2022-06-24 15:01:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const myName = "Habib Hakeem";
+const myHeight = "187cm";
+const myCountry = "Nigeria";
+const myState = "Abuja";
+
+alert("My name is " + myName+ "\nMy height is " + myHeight + "\nMy country is " + myCountry + "\nMy state of residence is " + myState)
